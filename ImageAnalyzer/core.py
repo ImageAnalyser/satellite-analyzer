7e096906ef9807ea50331b8d397384d31e8fb1fd
Imen Felhi <felhiimene@gmail.com>
2016-04-04 14:04:42 +0100

    update core.py

--- ImageAnalyzer/core.py
+++ ImageAnalyzer/core.py
@@ -15,4 +15,11 @@ from pylab import (
 class ImageAnalyzer:
     output_dir = 'ParaguayOut/'
 
+    def __init__(self, images,
+                 output_dir=output_dir,
+                 facteur=1000.0,
+                 centred=0,
+                 bande=0,
+                 verbosity=2
+                 ):
    
