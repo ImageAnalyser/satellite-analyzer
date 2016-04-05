1d299ef6e3caaf8cc671e767f47b3733d6cab68c
Imen Felhi <felhiimene@gmail.com>
2016-04-05 15:00:30 +0100

    add on_quit_clicked and on_clear_clear_clicked methods

--- ImageAnalyzer/eventHandler.py
+++ ImageAnalyzer/eventHandler.py
@@ -9,4 +9,21 @@ class EventHandler():
     def __init__(self, app):
         self.app = app
    
-    
+    def on_quit_clicked(self, *args):
+        """clean and close the app"""
+        Gtk.main_quit(*args)
+
+    def on_clear_clicked(self, *args):
+        """clear images list and image view
+        recuperation of default value from graphical object
+        """
+        while self.app.imageList.get_row_at_index(0):
+            self.app.imageList.get_row_at_index(0).destroy()
+        while self.app.resultList.get_row_at_index(0):
+            self.app.resultList.get_row_at_index(0).destroy()
+        old_viewport = self.app.imageScrolled.get_child()
+        if old_viewport:
+            old_viewport.destroy()
+        old_viewport = self.app.resultScrolled.get_child()
+        if old_viewport:
+            old_viewport.destroy()
